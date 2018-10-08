290ccf5826015a59f63d2b83a1b160b28307feb9
Quentin Prevost <quentin.prevost1495@gmail.com>
2018-10-08 13:20:13 +0200

    Revert "Update .gitignore"
    This reverts commit dc2d544513b3b3abf810ff1db5c0e0dff239613a.

--- MyWeatherApp/AppDelegate.swift
+++ MyWeatherApp/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let appConfig = AppConfig()
         appConfig.start()
         return true
